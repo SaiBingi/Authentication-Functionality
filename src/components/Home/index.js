e844e6d27eb2b064216d9f3c54bf89ba2bbd7d3c
Sai Bingi <saibingi1208@gmail.com>
2022-05-23 09:27:44 +0000

    Authentication Functionality Solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+import './index.css'
+
+const Home = () => (
+  <div className="home-section">
+    <Header />
+    <h1 className="home-route">Home Route</h1>
+    <LogoutButton />
+  </div>
+)
+
+export default Home
---
+++ src/components/LogoutButton/index.js
@@ -0,0 +1,19 @@
+import {withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import './index.css'
+
+const LogoutButton = props => {
+  const onClickLogoutButton = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+
+  return (
+    <button type="button" className="button" onClick={onClickLogoutButton}>
+      Logout
+    </button>
+  )
+}
+
+export default withRouter(LogoutButton)
